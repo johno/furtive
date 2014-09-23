7530d19f68f9045dabc4a424e0638b19a6f06624
John Otander <johnotander@gmail.com>
2014-09-22 23:45:31 -0600

    Add css build product.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var cssmin  = require('gulp-minify-css');
 gulp.task('scss', function() {
   return gulp.src('scss/all.scss')
     .pipe(sass())
-    .pipe(rename('gray.css'))
+    .pipe(rename('furtive.css'))
     .pipe(gulp.dest('css'))
     .pipe(cssmin())
     .pipe(rename({ suffix: '.min' }))
@@ -14,9 +14,9 @@ gulp.task('scss', function() {
 });
 
 gulp.task('min', function() {
-  return gulp.src('css/gray.scss')
+  return gulp.src('css/furtive.scss')
     .pipe(cssmin())
-    .pipe(rename('gray.min.css'))
+    .pipe(rename('furtive.min.css'))
     .pipe(gulp.dest('css'));
 });
 
