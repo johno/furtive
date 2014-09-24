38b0f05a97fd92fadc49277627c85f1f11c733e7
John Otander <johnotander@gmail.com>
2014-09-23 21:37:16 -0600

    Add tables.

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp    = require('gulp');
 var sass    = require('gulp-sass');
 var rename  = require('gulp-rename');
 var cssmin  = require('gulp-minify-css');
+var prefix  = require('gulp-autoprefixer');
 
 gulp.task('scss', function() {
   return gulp.src('scss/all.scss')
     .pipe(sass())
+    .pipe(prefix("last 1 version", "> 1%", "ie 10"))
     .pipe(rename('furtive.css'))
     .pipe(gulp.dest('css'))
     .pipe(cssmin())
