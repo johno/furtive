99f9d7b3c742df8b3fd0863f827c721b083e5931
John Otander <johnotander@gmail.com>
2014-09-29 22:38:02 -0600

    Add size breakdown.

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,17 @@ var sass    = require('gulp-sass');
 var rename  = require('gulp-rename');
 var cssmin  = require('gulp-minify-css');
 var prefix  = require('gulp-autoprefixer');
+var size    = require('gulp-size');
 
 gulp.task('scss', function() {
   return gulp.src('scss/all.scss')
     .pipe(sass())
+    .pipe(size())
     .pipe(prefix("last 1 version", "> 1%", "ie 10"))
     .pipe(rename('furtive.css'))
     .pipe(gulp.dest('css'))
     .pipe(cssmin())
+    .pipe(size())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('css'));
 });
